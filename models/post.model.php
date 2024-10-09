5b1e1e87acfd162b7e24bcd588d4d01bc80d111d
jason16fm <jehisonfm16@gmail.com>
2024-10-09 11:39:33 -0500

    Update post.model.php

--- models/post.model.php
+++ models/post.model.php
@@ -1,29 +1,30 @@
-<?php 
+<?php
 
 require_once "connection.php";
 
-class PostModel{
+class PostModel
+{
 
 	/*=============================================
 	Peticion POST para crear datos de forma dinámica
 	=============================================*/
 
-	static public function postData($table, $data){
+	static public function postData($table, $data)
+	{
 
 		$columns = "";
 		$params = "";
-		
-		
+
+
 
 		foreach ($data as $key => $value) {
-			
-			$columns .= $key.",";
-			
-			$params .= ":".$key.",";
-			
+
+			$columns .= $key . ",";
+
+			$params .= ":" . $key . ",";
 		}
-		
-		
+
+
 
 		$columns = substr($columns, 0, -1);
 		$params = substr($params, 0, -1);
@@ -36,28 +37,48 @@ class PostModel{
 
 		foreach ($data as $key => $value) {
 
-			$stmt->bindParam(":".$key, $data[$key], PDO::PARAM_STR);
-		
+			$stmt->bindParam(":" . $key, $data[$key], PDO::PARAM_STR);
 		}
 
-		if($stmt -> execute()){
+		if ($stmt->execute()) {
 
 			$response = array(
 
 				"lastId" => $link->lastInsertId(),
 				"comment" => "The process was successful"
-				
+
 			);
 
 			return $response;
-		
-		}else{
+		} else {
 
 			return $link->errorInfo();
-
 		}
-
-
 	}
 
+	/*=============================================
+	  Guardar token en base de datos
+	=============================================*/
+
+	function saveTokenToDatabase($email, $code, $expiration)
+	{
+		$connection = new Connection(); // 
+		$db = $connection->connect();
+
+		$stmt = $db->prepare("UPDATE users SET reset_code_user = ?, reset_code_exp_user = ? WHERE email_user = ?");
+		$stmt->execute([$code, $expiration, $email]);
+	}
+
+	/*=============================================
+	  Actualizar contraseña en base de datos
+	=============================================*/
+
+	function updatePasswordInDatabase($email, $hashedPassword)
+	{
+		$connection = new Connection();
+		$db = $connection->connect();
+
+		$stmt = $db->prepare("UPDATE users SET password_user = ? WHERE email_user = ?");
+		$stmt->execute([$hashedPassword, $email]);
+	}
 }
