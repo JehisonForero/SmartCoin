9e345b622f624069929273b1757cc2a3281926d3
Mora070 <Ejmora@jdc.edu.co>
2024-11-15 07:34:46 -0500

    cors

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ CORS
 =============================================*/
 
 header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization");
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Credentials:true');
 header('content-type: application/json; charset=utf-8');
